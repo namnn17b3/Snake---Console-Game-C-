99abb7e94df4ea5bcd7705bd2444ff63628bd2e2
Nguyễn Ngọc Nam <88151568+nam29052002@users.noreply.github.com>
2023-02-01 17:44:36 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "DoHoa.h"
 
-bool cmp(pair<string, int> alpha, pair<string, int> beta)
-{
+bool cmp(pair<string, int> alpha, pair<string, int> beta) {
     if (alpha.second > beta.second)
         return true;
     if (alpha.second == beta.second)
@@ -9,8 +8,7 @@ bool cmp(pair<string, int> alpha, pair<string, int> beta)
     return false;
 }
 
-struct Point
-{
+struct Point {
     int x, y;
     char huong;
 };
@@ -41,30 +39,24 @@ string level_string = "2. NORMAL";
 string status[] = {"PLAYING...", "PAUSING...", "GAME OVER "};
 deque<Point> Q;
 
-int Random(int minN, int maxN)
-{
+int Random(int minN, int maxN) {
     return minN + rand() % (maxN - minN + 1);
 }
 
-Point RandomPoint()
-{
+Point RandomPoint() {
     Point res;
-    do
-    {
+    do {
         res.x = Random(1, 65);
         res.y = Random(1, 34);
-    }
-    while (exist.find(res.x * N + res.y) != exist.end());
+    } while (exist.find(res.x * N + res.y) != exist.end());
     return res;
 }
 
-void SoundEffect()
-{
+void SoundEffect() {
     PlaySound(TEXT("effect.wav"), NULL, SND_LOOP);
 }
 
-void AddFront()
-{
+void AddFront() {
     if (Q.front().huong == 'a')
         Q.push_front({Q.front().x - 1, Q.front().y, 'a'});
     else if (Q.front().huong == 'd')
@@ -75,8 +67,7 @@ void AddFront()
         Q.push_front({Q.front().x, Q.front().y + 1, 's'});
 }
 
-void Play()
-{
+void Play() {
     exist.clear();
     Q.clear();
     score = 0;
@@ -85,23 +76,20 @@ void Play()
     SetFontSize(20);
     SetColor(10, 5);
     ResizeConsole(1000, 800);
-    for (int i = 0; i < 38; i++)
-    {
+    for (int i = 0; i < 38; i++) {
         Gotoxy(0, 0 + i);
         cout << "░" << endl;
         Gotoxy(70, 0 + i);
         cout << "░" << endl;
     }
-    for (int i = 0; i < 70; i++)
-    {
+    for (int i = 0; i < 70; i++) {
         Gotoxy(0 + i, 0);
         cout << "░";
         Gotoxy(0 + i, 37);
         cout << "░";
     }
     SetColor(11, 4);
-    for (int i = 0; i < 13; i++)
-    {
+    for (int i = 0; i < 13; i++) {
         Gotoxy(73, 0 + i);
         for (int j = 0; j < 27; j++)
             cout << ' ';
@@ -124,8 +112,7 @@ void Play()
     Gotoxy(73, 33);
     cout << "===========================" << endl;
     SetColor(6, 4);
-    for (int i = 0; i < 16; i++)
-    {
+    for (int i = 0; i < 16; i++) {
         Gotoxy(73, 9 + i);
         for (int j = 0; j < 27; j++)
             cout << ' ';
@@ -185,17 +172,14 @@ void Play()
     cout << heart;
     Gotoxy(0, 0);
     Sleep(700);
-    while (true)
-    {
-        if (kbhit())
-        {
+    while (true) {
+        if (kbhit()) {
             huong = getch();
             if (huong != 'a' && huong != 'd' && huong != 's' && huong != 'w'
-                && huong != 'p' && huong != 'r' && huong != 'b')
-                    huong = old_huong;
+                    && huong != 'p' && huong != 'r' && huong != 'b')
+                huong = old_huong;
 
-            if (huong == 'p')
-            {
+            if (huong == 'p') {
                 SetColor(6, 15);
                 Gotoxy(82, 17);
                 cout << "Continue: C" << endl;
@@ -203,30 +187,21 @@ void Play()
                 SetColor(6, 4);
                 cout << "Status: " << status[1] << endl;
                 while ((huong = getch()) != 'c');
-            }
-            else if (huong == 'a' || huong == 'd' || huong == 'w' || huong == 's')
-            {
+            } else if (huong == 'a' || huong == 'd' || huong == 'w' || huong == 's') {
                 if ((huong == 'a' && old_huong != 'a' && old_huong != 'd') || (huong == 'd' && old_huong != 'a' && old_huong != 'd')
-                    || (huong == 's' && old_huong != 's' && old_huong != 'w') || (huong == 'w' && old_huong != 's' && old_huong != 'w'))
-                {
+                        || (huong == 's' && old_huong != 's' && old_huong != 'w') || (huong == 'w' && old_huong != 's' && old_huong != 'w')) {
                     Point tmp = Q.front();
                     Q.pop_front();
                     Q.push_front({tmp.x, tmp.y, huong});
-                }
-                else
-                {
+                } else {
                     while (kbhit()) getch();
                 }
-            }
-            else if (huong == 'r')
-            {
+            } else if (huong == 'r') {
                 score_table[current_player] = current_high_score;
                 WriteData();
                 Play();
                 break;
-            }
-            else if (huong == 'b')
-            {
+            } else if (huong == 'b') {
                 score_table[current_player] = current_high_score;
                 WriteData();
                 SetFontSize(8);
@@ -252,8 +227,7 @@ void Play()
         AddFront();
 
         if (Q.front().x > 0 && Q.front().x < 70 && Q.front().y > 0 && Q.front().y < 37
-            && exist.find(Q.front().x * N + Q.front().y) == exist.end())
-        {
+                && exist.find(Q.front().x * N + Q.front().y) == exist.end()) {
             Gotoxy(before_front.x, before_front.y);
             SetColor(12, 7);
             cout << ' ';
@@ -272,8 +246,7 @@ void Play()
             before_front = Q.front();
 
             exist.insert(Q.front().x * N + Q.front().y);
-            if (Q.front().x == food.x && Q.front().y == food.y)
-            {
+            if (Q.front().x == food.x && Q.front().y == food.y) {
                 thread sound_effect(SoundEffect);
                 sound_effect.detach();
 
@@ -306,25 +279,18 @@ void Play()
                 cout << heart;
             }
             Sleep(level);
-        }
-        else
-        {
+        } else {
             Gotoxy(78, 23);
             SetColor(6, 4);
             cout << "Status: " << status[2] << endl;
-            do
-            {
+            do {
                 huong = getch();
-            }
-            while (huong != 'r' && huong != 'b');
-            if (huong == 'r')
-            {
+            } while (huong != 'r' && huong != 'b');
+            if (huong == 'r') {
                 score_table[current_player] = current_high_score;
                 WriteData();
                 Play();
-            }
-            else if (huong == 'b')
-            {
+            } else if (huong == 'b') {
                 score_table[current_player] = current_high_score;
                 WriteData();
                 SetFontSize(8);
@@ -338,34 +304,29 @@ void Play()
     }
 }
 
-void NewGame()
-{
+void NewGame() {
     SetFontSize(20);
     SetColor(10, 5);
-    for (int i = 0; i < 24; i++)
-    {
+    for (int i = 0; i < 24; i++) {
         Gotoxy(1, 20 + i);
         cout << "░" << endl;
         Gotoxy(90, 20 + i);
         cout << "░" << endl;
     }
-    for (int i = 0; i < 89; i++)
-    {
+    for (int i = 0; i < 89; i++) {
         Gotoxy(2 + i, 20);
         cout << "░";
         Gotoxy(2 + i, 43);
         cout << "░";
     }
     SetColor(14, 5);
-    for (int i = 0; i < 5; i++)
-    {
+    for (int i = 0; i < 5; i++) {
         Gotoxy(25, 29 + i);
         cout << "░" << endl;
         Gotoxy(65, 29 + i);
         cout << "░" << endl;
     }
-    for (int i = 0; i < 40; i++)
-    {
+    for (int i = 0; i < 40; i++) {
         Gotoxy(25 + i, 29);
         cout << "░";
         Gotoxy(25 + i, 33);
@@ -374,16 +335,14 @@ void NewGame()
     Gotoxy(38, 28);
     SetColor(11, 4);
     cout << "NEW PLAYER NAME\n";
-    while (true)
-    {
+    while (true) {
         ShowCur(true);
         Gotoxy(26, 31);
         SetColor(0, 15);
         string player_name;
         getline(cin, player_name);
         ShowCur(false);
-        if (score_table.find(player_name) != score_table.end())
-        {
+        if (score_table.find(player_name) != score_table.end()) {
             Gotoxy(35, 35);
             SetColor(0, 15);
             cout << "PLAYER ALREADY EXISTS" << endl;
@@ -391,9 +350,7 @@ void NewGame()
             cout << "                                       ";
             Gotoxy(26, 31);
             continue;
-        }
-        else
-        {
+        } else {
             current_player = player_name;
             current_high_score = 0;
             score_table[current_player] = 0;
@@ -405,63 +362,52 @@ void NewGame()
     }
 }
 
-void ContinueGame()
-{
+void ContinueGame() {
     SetFontSize(20);
     SetColor(10, 5);
-    for (int i = 0; i < 24; i++)
-    {
+    for (int i = 0; i < 24; i++) {
         Gotoxy(1, 20 + i);
         cout << "░" << endl;
         Gotoxy(90, 20 + i);
         cout << "░" << endl;
     }
-    for (int i = 0; i < 89; i++)
-    {
+    for (int i = 0; i < 89; i++) {
         Gotoxy(2 + i, 20);
         cout << "░";
         Gotoxy(2 + i, 43);
         cout << "░";
     }
-    if (vt.size() > 0)
-    {
-        for (int i = 0; i < vt.size(); i++)
-        {
+    if (vt.size() > 0) {
+        for (int i = 0; i < vt.size(); i++) {
             if (i == 0) SetColor(14, 5);
             else SetColor(0, 7);
             Gotoxy(38, 28 + i * 2);
             cout << i + 1 << "." << vt[i].first << endl;
         }
         int cnt = 0;
-        while (true)
-        {
+        while (true) {
             char c = getch();
             if (c == 13) break;
             if (c != -32) continue;
             c = getch();
             if (c == 72) cnt = (cnt - 1 + vt.size()) % vt.size();
             else if (c == 80) cnt = (cnt + 1 + vt.size()) % vt.size();
-            for (int i = 0; i < vt.size(); i++)
-            {
+            for (int i = 0; i < vt.size(); i++) {
                 if (i != cnt) SetColor(0, 7);
                 else SetColor(14, 5);
                 Gotoxy(38, 28 + i * 2);
                 cout << i + 1 << "." << vt[i].first << endl;
             }
         }
-        for (int i = 0; i < vt.size(); i++)
-        {
-            if (i == cnt)
-            {
+        for (int i = 0; i < vt.size(); i++) {
+            if (i == cnt) {
                 current_player = vt[i].first;
                 current_high_score = vt[i].second;
                 break;
             }
         }
         Play();
-    }
-    else
-    {
+    } else {
         Gotoxy(34, 30);
         SetColor(0, 15);
         cout << "LIST HIGH SCORE IS EMPTY" << endl;
@@ -475,28 +421,24 @@ void ContinueGame()
     }
 }
 
-void HighScore()
-{
+void HighScore() {
     ResizeConsole(400, 250);
     SetFontSize(20);
     SetColor(10, 5);
-    for (int i = 0; i < 24; i++)
-    {
+    for (int i = 0; i < 24; i++) {
         Gotoxy(1, 20 + i);
         cout << "░" << endl;
         Gotoxy(90, 20 + i);
         cout << ' ' << endl;
     }
-    for (int i = 0; i < 89; i++)
-    {
+    for (int i = 0; i < 89; i++) {
         Gotoxy(2 + i, 20);
         cout << "░";
         Gotoxy(2 + i, 43);
         cout << "░";
     }
     SetColor(0, 12);
-    for (int i = 0; i < vt.size(); i++)
-    {
+    for (int i = 0; i < vt.size(); i++) {
         Gotoxy(38, 28 + i * 2);
         cout << i + 1 << "." << vt[i].first << ": " << vt[i].second << endl;
     }
@@ -508,44 +450,38 @@ void HighScore()
     MainMenu();
 }
 
-void PlayGame()
-{
+void PlayGame() {
     ResizeConsole(400, 250);
     SetFontSize(20);
     SetColor(10, 5);
-    for (int i = 0; i < 24; i++)
-    {
+    for (int i = 0; i < 24; i++) {
         Gotoxy(1, 20 + i);
         cout << "░" << endl;
         Gotoxy(90, 20 + i);
         cout << "░" << endl;
     }
-    for (int i = 0; i < 89; i++)
-    {
+    for (int i = 0; i < 89; i++) {
         Gotoxy(2 + i, 20);
         cout << "░";
         Gotoxy(2 + i, 43);
         cout << "░";
     }
     string choose[] = {"1. NEW GAME\n", "2. CONTINUE\n", "3. BACK\n"};
-    for (int i = 0; i < 3; i++)
-    {
+    for (int i = 0; i < 3; i++) {
         if (i == 0) SetColor(14, 5);
         else SetColor(0, 7);
         Gotoxy(40, 29 + 2 * i);
         cout << choose[i];
     }
     int cnt = 0;
-    while (true)
-    {
+    while (true) {
         char c = getch();
         if (c == 13) break;
         if (c != -32) continue;
         c = getch();
         if (c == 72) cnt = (cnt + 2) % 3;
         else if (c == 80) cnt = (cnt + 4) % 3;
-        for (int i = 0; i < 3; i++)
-        {
+        for (int i = 0; i < 3; i++) {
             if (i != cnt) SetColor(0, 7);
             else SetColor(14, 5);
             Gotoxy(40, 29 + 2 * i);
@@ -559,44 +495,38 @@ void PlayGame()
     else if (cnt == 2) MainMenu();
 }
 
-void ChooseLevel()
-{
+void ChooseLevel() {
     ResizeConsole(400, 250);
     SetFontSize(20);
     SetColor(10, 5);
-    for (int i = 0; i < 24; i++)
-    {
+    for (int i = 0; i < 24; i++) {
         Gotoxy(1, 20 + i);
         cout << "░" << endl;
         Gotoxy(90, 20 + i);
         cout << "░" << endl;
     }
-    for (int i = 0; i < 89; i++)
-    {
+    for (int i = 0; i < 89; i++) {
         Gotoxy(2 + i, 20);
         cout << "░";
         Gotoxy(2 + i, 43);
         cout << "░";
     }
     string choose[] = {"1. EASY\n", "2. NORMAL\n", "3. DIFFICULT\n"};
-    for (int i = 0; i < 3; i++)
-    {
+    for (int i = 0; i < 3; i++) {
         if (i == 0) SetColor(14, 5);
         else SetColor(0, 7);
         Gotoxy(40, 29 + 2 * i);
         cout << choose[i];
     }
     int cnt = 0;
-    while (true)
-    {
+    while (true) {
         char c = getch();
         if (c == 13) break;
         if (c != -32) continue;
         c = getch();
         if (c == 72) cnt = (cnt + 2) % 3;
         else if (c == 80) cnt = (cnt + 4) % 3;
-        for (int i = 0; i < 3; i++)
-        {
+        for (int i = 0; i < 3; i++) {
             if (i != cnt) SetColor(0, 7);
             else SetColor(14, 5);
             Gotoxy(40, 29 + 2 * i);
@@ -612,8 +542,7 @@ void ChooseLevel()
     MainMenu();
 }
 
-void MainMenu()
-{
+void MainMenu() {
     SetConsoleOutputCP(65001);
     ShowCur(false);
     DisableSelection();
@@ -633,32 +562,28 @@ void MainMenu()
                             "Y~                P@@@&:    J@@@@~          !&:   :@7::::::::::7@@@@J     ~@@@@J::::^5G^      .#@@@B::::::::::::.\n",
                             "&J                P@@@&:    J@@@@~          !@:   :@~          !@@@@J     !@@@@?      7#J.    .&@@@G           5J\n",
                             "PPYYYYYYYYYYYYYYYYPBBBG. ^YYPBBBB5YJ.    ^YY5G5YY.:G5YYYYYYYYYY5BBBBPYY^JY5BBBBPYY:  :YPB5Y57 .PBBBGYYYYYYYYYYYGJ\n"
-                            };
+                           };
 
     SetColor(11, 4);
-    for (int i = 0; i < 10; i++)
-    {
+    for (int i = 0; i < 10; i++) {
         Gotoxy(63, 0 + i);
         cout << menu_title[i];
     }
     SetColor(10, 5);
-    for (int i = 0; i < 50; i++)
-    {
+    for (int i = 0; i < 50; i++) {
         Gotoxy(50, 20 + i);
         cout << "░" << endl;
         Gotoxy(189, 20 + i);
         cout << "░" << endl;
     }
-    for (int i = 1; i < 140; i++)
-    {
+    for (int i = 1; i < 140; i++) {
         Gotoxy(49 + i, 20);
         cout << "░";
         Gotoxy(49 + i, 69);
         cout << "░";
     }
     string choose[] = {"1. PLAY\n", "2. LEVEL\n", "3. HIGH SCORE\n", "4. QUIT\n"};
-    for (int i = 0; i < 4; i++)
-    {
+    for (int i = 0; i < 4; i++) {
         Gotoxy(112, 40 + i * 5);
         if (i != 0) SetColor(0, 7);
         else SetColor(14, 5);
@@ -667,16 +592,14 @@ void MainMenu()
     SetColor(0, 7);
     Gotoxy(0, 0);
     int cnt = 0;
-    while (true)
-    {
+    while (true) {
         char c = getch();
         if (c == 13) break;
         if (c != -32) continue;
         c = getch();
         if (c == 72) cnt = (cnt + 3) % 4;
         else if (c == 80) cnt = (cnt + 5) % 4;
-        for (int i = 0; i < 4; i++)
-        {
+        for (int i = 0; i < 4; i++) {
             Gotoxy(112, 40 + i * 5);
             if (i != cnt) SetColor(0, 7);
             else SetColor(14, 5);
@@ -690,32 +613,27 @@ void MainMenu()
     else if (cnt == 3) return;
 }
 
-int ParseInt(string s)
-{
+int ParseInt(string s) {
     int res = 0;
     for (char c : s)
         res = res * 10 + int(c) - 48;
     return res;
 }
 
-void LoadData()
-{
+void LoadData() {
     ifstream input("high_score.txt", ios_base::in);
     string s, name, str;
     int x;
-    while (getline(input, s))
-    {
+    while (getline(input, s)) {
         stringstream ss(s);
         vector<string> tmp;
         while (ss >> str) tmp.push_back(str);
         name = "";
-        for (int i = 0; i < tmp.size() - 1; i++)
-        {
+        for (int i = 0; i < tmp.size() - 1; i++) {
             name = name + tmp[i];
             if (i < tmp.size() - 2) name = name + " ";
         }
-        if (tmp.size() >= 2)
-        {
+        if (tmp.size() >= 2) {
             x = ParseInt(tmp.back());
             score_table[name] = x;
         }
@@ -727,8 +645,7 @@ void LoadData()
     input.close();
 }
 
-void WriteData()
-{
+void WriteData() {
     ofstream output("high_score.txt", ios_base::out);
     vt.clear();
     for (pair<string, int> p : score_table)
@@ -741,8 +658,7 @@ void WriteData()
     output.close();
 }
 
-int main()
-{
+int main() {
     LoadData();
     MainMenu();
 
